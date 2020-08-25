15a78c725b421e35333d11b4652dc8b609779105
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-25 12:06:11 -0400

    add seed file

---
+++ seed.sql
@@ -0,0 +1,23 @@
+USE employee_DB;
+
+--Employee Seeds--
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Betty", "White", 1, null);
+
+
+
+
+
+--Employee Role Seeds--
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Assistant", 42000, 1);
+
+
+
+
+--Department Seeds--
+
+INSERT INTO department (id, name)
+VALUES (1, "Software Development");
