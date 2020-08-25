9fdba7e0de2a213c0cad288fcee466d931ba552d
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-25 10:52:53 -0400

    db connection

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "mirage",
+    database: "employee_tracker",
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
