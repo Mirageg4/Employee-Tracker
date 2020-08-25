af011b694e4d2576e0c3ebf73eecb2ebc8923669
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-25 16:04:34 -0400

    update seed file

--- seed.sql
+++ seed.sql
@@ -3,22 +3,22 @@ USE employee_db;
 --Employee Seeds--
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Sherman", "Potter", 1, null);
+VALUES (1, "Sherman", "Potter", 1, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Ben", "Pierce", 1, null);
+VALUES (2, "Ben", "Pierce", 2, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (3, "BJ", "Hunnicut", 1, null);
+VALUES (3, "BJ", "Hunnicut", 3, 2);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (4, "Francis", "Mulcahy", 1, null);
+VALUES (4, "Francis", "Mulcahy", 4, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (5, "Francis", "Mulcahy", 1, null);
+VALUES (6, "Margaret", "Houlihan", 5, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (6, "Margaret", "Houlihan", 1, null);
+VALUES (6, "Walter", "O'Reilly", 6, 5);
 
 
 
@@ -28,19 +28,19 @@ INSERT INTO role (id, title, salary, department_id)
 VALUES (1, "CEO", 100000, 1);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (2, "Accountant", 100000, 1);
+VALUES (2, "Accountant", 90000, 1);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (3, "Salesman", 100000, 1);
+VALUES (3, "Salesman", 80000, 2);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (4, "IT Manager", 100000, 1);
+VALUES (4, "IT Manager", 70000, 3);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (5, "HR Director", 100000, 1);
+VALUES (5, "HR Director", 60000, 4);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (6, "Production Manager", 100000, 1);
+VALUES (6, "Production Manager", 50000, 5);
 
 --Department Seeds--
 
@@ -54,4 +54,7 @@ INSERT INTO department (id, name)
 VALUES (3, "IT");
 
 INSERT INTO department (id, name)
-VALUES (4, "IT");
+VALUES (4, "HR");
+
+INSERT INTO department (id, name)
+VALUES (5, "Production");
