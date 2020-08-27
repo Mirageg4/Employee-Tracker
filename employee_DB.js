a8006e410dbdfb152e65a1147db528e27fae4f14
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-27 15:24:41 -0400

    add employee functions

--- employee_DB.js
+++ employee_DB.js
@@ -1,7 +1,7 @@
 const connection = require("./db.js");
 const inquirer = require("inquirer");
 
-employeeOptions()
+employeeOptions();
 
 const mysql = require("mysql");
 const conTable = require("console.table");
@@ -27,30 +27,64 @@ async function employeeOptions() {
         console.log(employeeOptions);
     
         switch (employeeOptions) {
-            case 'View all employees':
+            case 'View All Employees':
                 viewEmployees();
                 break;
     
-            case 'View all roles':
+            case 'View Employee Roles':
                 viewRoles();
                 break;
-    
+
+            case 'View Departments':
+                viewDepartments();
+                break;  
+                
+            case 'Add Employee':
+            addEmployee();
+            break;      
+            
+            case 'Add Employee Role':
+            addRole();
+            break;
+
+            case 'Add Department':
+            addEmployee();
+            break;
+
+            case 'Update Employee Role':
+            updateRole();
+            break;
+
+            case 'Remove Employee':
+            removeEmployee();
+            break;
         }
     })
         
 
 };
 
-// // Allows user to VIEW ALL EMPLOYEES currently in the database
+// Allow user to view all employees
 function viewEmployees() {
-    console.log('Employees currently in database');
+    console.log('Employees in database');
    let query =
         'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id;';
     connection.query(query, function (err, optionChoice) {
-        console.log('\n Employees currently in database \n');
+        console.log('\n Employees in database \n');
         console.table(optionChoice);
     });
     
+    employeeOptions();
 
+}
+
+// Allow user to view employee roles
+function viewRoles() {
+    connection.query('SELECT * from role', function (err, optionChoice) {
+        console.log('\n Employee Roles in database \n');
+        console.table(optionChoice);
+    });
+    
+    employeeOptions();
 
 }
